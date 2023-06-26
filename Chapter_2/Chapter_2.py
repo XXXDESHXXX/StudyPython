d2d8ddddce8aea79193c1e34fe19161e12050194
XXXDESHXXX <kirya.shvarts@list.ru>
2023-06-26 14:24:49 +0300

    init

---
+++ Chapter_2/Chapter_2.py
@@ -0,0 +1,172 @@
+# ***Algorithmic simulator***
+# Task 1
+# height = input("Write your height:")
+# print(height)
+# Task 2
+# color = input("Write your favorite color:")
+# print(color)
+# Task 3
+# a = 3
+# b = a + 2
+# print(b)
+#
+# b = 4
+# a = b * 4
+# print(a)
+#
+# a = 5
+# b = a / 3.14
+# print(b)
+#
+# b = 15
+# a = b - 8
+# print(a)
+# Task 4
+# w = 5
+# x = 4
+# y = 8
+# z = 2
+# result = x + y
+# result = z * 2
+# result = y / x
+# result = y - z
+# result = w // z
+# print(type(result), result)
+# Task 5
+# total = 10 + 14
+# print(total)
+# Task 6
+# down_payment = 330
+# total = 1000
+# due = total - down_payment
+# print(due)
+# Task 7
+# subtotal = 100.75
+# total = subtotal * 0.15
+# print (total)
+# Task 8
+# a = 5
+# b = 2
+# c = 3
+# result = a + b * c
+# print(result)
+# Task 9
+# num = 99
+# num = 5
+# print(num)
+# Task 10
+# sales = 12345.12345
+# print(format(sales, ".2f"))
+# Task 11
+# number = 1234567.456
+# print(format(number, ",.1f"))
+# Task 12
+# print('Джордж', 'Джон', 'Пол', 'Ринго', sep='@')
+# Task 13
+# import turtle
+# turtle.circle(75)
+# Task 14
+# import turtle
+# turtle.fillcolor('blue')
+# turtle.begin_fill()
+# turtle.forward(100)
+# turtle.dot()
+# turtle.setheading(90)
+# turtle.forward(100)
+# turtle.dot()
+# turtle.setheading(180)
+# turtle.forward(100)
+# turtle.dot()
+# turtle.setheading(270)
+# turtle.forward(100)
+# turtle.dot()
+# turtle.setheading(360)
+# turtle.end_fill()
+# Task 15
+# import turtle
+# turtle.forward(100)
+# turtle.dot()
+# turtle.setheading(90)
+# turtle.forward(100)
+# turtle.dot()
+# turtle.setheading(180)
+# turtle.forward(100)
+# turtle.dot()
+# turtle.setheading(270)
+# turtle.forward(100)
+# turtle.dot()
+# turtle.setheading(360)
+# turtle.forward(50)
+# turtle.dot()
+# turtle.fillcolor('red')
+# turtle.begin_fill()
+# turtle.circle(50)
+# turtle.end_fill()
+# ***Programming tasks***
+# Task 1
+# name = input("Write your name")
+# address = input("Write your address, with your town, area and postal code")
+# telephon = input("Write your telephon number")
+# specialization = input("Write your specialization in your educational institution")
+# print(name)
+# print(address)
+# print(telephon)
+# print(specialization)
+# Task 2
+# total_sales_amount = float(input("Write total sales amount"))
+# int(total_sales_amount)
+# profit = total_sales_amount * 0.23
+# print("Total profit is:", profit)
+# Task 3
+# Square_meters_of_land = float(input("Write total number of square meters of land"))
+# print("Total number of acres is:", Square_meters_of_land / 4047)
+# Task 4
+# first_goods = float(input("Write cost of your first goods"))
+# second_goods = float(input("Write cost of your second goods"))
+# third_goods = float(input("Write cost of your third goods"))
+# fourth_goods = float(input("Write cost of your fourth goods"))
+# fifth_goods = float(input("Write cost of your fifth goods"))
+# cost_of_all_goods = first_goods + second_goods + third_goods + fourth_goods + fifth_goods
+# sales_tax = cost_of_all_goods * 0.7
+# total_amount = cost_of_all_goods + sales_tax
+# print("cost of all goods is:", cost_of_all_goods, "sales_tax is:", sales_tax, "total amount is:", total_amount)
+# Task 5
+# speed = 70
+# time = float(input("Write time:"))
+# distance = speed * time
+# print("Distance is:", distance)
+# Task 6
+# purchase_amount = float(input("Write purchase amount:"))
+# federal_tax = purchase_amount * 0.05
+# regional_tax = purchase_amount * 0.025
+# total_tax = federal_tax + regional_tax
+# total_amount = purchase_amount + total_tax
+# print("Purchase amount is:", purchase_amount, "\n federal tax is:", federal_tax, "\n regional tax is:", regional_tax)
+# print("total amount is:", total_amount, "\n total tax is:", total_tax)
+# Task 7
+# kilometers_traveled = float(input("Write traveled kilometers:"))
+# gasoline_consumption = float(input("Write gasoline consumption:"))
+# print("Consumption is:", kilometers_traveled / gasoline_consumption)
+# Task 8
+# food_cost = float(input('Food cost is:'))
+# tips = food_cost * 0.18
+# tax = food_cost * 0.07
+# total_cost = food_cost + tips + tax
+# print('Food cost is:', food_cost, 'tips is:', tips, 'tax is:', tax, 'total_cost is:', total_cost)
+# Task 9
+# temperature_celsius = int(input('Write temperature in celsius:'))
+# temperature_fahrenheit = 9/5 * temperature_celsius + 32
+# print(temperature_fahrenheit)
+# Task 10
+# sugar = 1.5
+# butter = 1
+# flour = 2.75
+# maffin = (int(input('Write amount of maffin')))
+# print('Sugar is:', maffin * (1.5/48))
+# print('Sugar is:', maffin * (1/48))
+# print('Sugar is:', maffin * (2.75/48))
+# Task 11
+# boys = int(input('Write number of boys'))
+# girls = int(input('Write number of girls'))
+# students = boys + girls
+# print('procent of boys is:', boys/students, 'procent of girls is:', girls/students)
