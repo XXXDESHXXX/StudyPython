4357ccf06558d55d36c6dd3db098788d5a6c2407
XXXDESHXXX <kirya.shvarts@list.ru>
2023-06-27 23:29:37 +0300

    Finish init

--- Chapter_3/Chapter_3.py
+++ Chapter_3/Chapter_3.py
@@ -1,1 +1,197 @@
+# ***Algorithmic simulator***
+# Task 1
+# x = int(input('x = '))
+# if x > 100:
+#     y = 20
+#     z = 40
+#     print(z)
+#     print(y)
+# Task 2
+# a = int(input('a = '))
+# if a < 10:
+#     b = 0
+#     c = 1
+#     print(c)
+#     print(b)
+# Task 3
+# a = int(input('a = '))
+# if a < 10:
+#     b = 0
+#     print(b)
+# else:
+#     b = 99
+#     print(b)
+# Task 4
+# score = int(input('Write your score: '))
+# A_score = 80
+# B_score = 70
+# C_score = 50
+# D_score = 40
+# if score >= A_score:
+#     print('Your level - А.')
+# else:
+#     if score >= B_score:
+#         print('Your level - В. ')
+#     else:
+#         if score >= C_score:
+#             print('Your level - С. ')
+#         else:
+#             if score >= D_score:
+#                 print('Your level - D.')
+#             else:
+#                 print('Your level - F. ')
+# Task 5
+# amount1 = int(input('amount1 = '))
+# amount2 = int(input('amount2 = '))
+# if amount1 > 10 and amount2 < 100:
+#     if amount1 > amount2:
+#         print(amount1)
+#     else:
+#         print(amount2)
+# Task 6
+# speed = int(input('speed = '))
+# if 24 < speed < 56:
+#     print('Speed is normal')
+# else:
+#     print('emergency speed')
+# Task 7
+# points = int(input('Point = '))
+# if 9 < points < 51:
+#     print('Uncorrected points')
+# else:
+#     print('Correct!')
+# ***Programming tasks***
+# Task 1
+# number = int(input('Write number of your week day'))
+# if number == 1:
+#     print("It's Monday")
+# elif number == 2:
+#     print("It's Tuesday")
+# elif number == 3:
+#     print("It's Wednesday")
+# elif number == 4:
+#     print("It's Thursday")
+# elif number == 5:
+#     print("It's Friday")
+# elif number == 6:
+#     print("It's Saturday")
+# elif number == 7:
+#     print("It's Sunday")
+# else:
+#     print('Your number is incorrect!')
+# Task 2
+# first_width = float(input('Width of the first rectangle '))
+# first_height = float(input('Height of the first rectangle'))
+# second_width = float(input('Width of the second rectangle'))
+# second_height = float(input('Height of the second rectangle'))
+# first_square = first_height * first_width
+# second_square = second_height * second_width
+# if first_square == second_square:
+#     print('Equal')
+# elif first_square > second_square:
+#     print('First is higher')
+# elif second_square > first_square:
+#     print('Second is higher')
+# Task 3
+# age = float(input('Write your age '))
+# if age <= 1:
+#     print("It's baby")
+# elif 13 >= age > 1:
+#     print("It's kid")
+# elif 13 < age < 20:
+#     print("It's teenager")
+# elif age >= 20:
+#     print("It's Adult")
+# Task 4
+# number = int(input('Write number '))
+# if number == 1:
+#     print('I')
+# elif number == 2:
+#     print('II')
+# elif number == 3:
+#     print('III')
+# elif number == 4:
+#     print('IV')
+# elif number == 5:
+#     print('V')
+# elif number == 6:
+#     print('VI')
+# elif number == 7:
+#     print('VII')
+# elif number == 8:
+#     print('VIII')
+# elif number == 9:
+#     print('IX')
+# elif number == 10:
+#     print('X')
+# else:
+#     print('Error')
+# Task 5
+# mass = float(input('Your mass is: '))
+# weight = mass * 9.8
+# if weight > 500:
+#     print('Your weight is high')
+# elif weight < 100:
+#     print('Your weight is light')
+# Task 6
+# month = int(input('Write your month'))
+# day = int(input('Write your day'))
+# year = int(input('Write your year(2 number)'))
+# if day * month == year:
+#     print("It's magic data!")
+# else:
+#     print("It's not magic data(")
+# Task 7
+# first_color = input('Write your color for mix')
+# second_color = input('Write your second color for mix')
+# if first_color == 'Red' and second_color == 'Blue':
+#     print('Violet')
+# if second_color == 'Red' and first_color == 'Blue':
+#     print('Violet')
+# elif second_color == 'Red' and first_color == 'Yellow':
+#     print('Orange')
+# elif first_color == 'Red' and second_color == 'Yellow':
+#     print('Orange')
+# elif first_color == 'Blue' and second_color == 'Yellow':
+#     print('Green')
+# elif second_color == 'Blue' and first_color == 'Yellow':
+#     print('Green')
+# else:
+#     print('Error')
+# Task 8
+# people = int(input('Amount of people: '))
+# hot_dogs = int(input('Amount of hot dogs for every people: '))
+# sausages = hot_dogs * people
+# buns = sausages * 2
+# packet_of_sausages = sausages / 10
+# packet_of_buns = buns / 8
+# remains_of_sausages = sausages % 10
+# remains_of_buns = buns % 8
+# print('Amount of remains buns is: ', remains_of_buns)
+# print('Amount of remains sausages is: ', remains_of_sausages)
+# print('Necessary packet of sausages is: ', packet_of_sausages)
+# print('Necessary packet of buns is: ', packet_of_buns)
+# Task 9
+# pocket = int(input("Your pocket is: "))
+# if pocket == 0:
+#     print("Green")
+# if 1 < pocket < 10 and pocket % 2 == 0:
+#     print("Black")
+# elif 10 > pocket > 1 == pocket % 2:
+#     print("Red")
+# elif 11 < pocket < 18 and pocket % 2 == 0:
+#     print("Red")
+# elif 11 < pocket < 18 and pocket % 2 == 1:
+#     print("Black")
+# elif 19 < pocket < 28 and pocket % 2 == 1:
+#     print("Red")
+# elif 19 < pocket < 28 and pocket % 2 == 0:
+#     print("Black")
+# elif 29 < pocket < 36 and pocket % 2 == 1:
+#     print("Black")
+# elif 29 < pocket < 36 and pocket % 2 == 0:
+#     print("Red")
+# else:
+#     print("Error")
+# Task 10
 
