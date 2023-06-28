3204e4738241ef71d8c2d5517475ab17dbe73e98
XXXDESHXXX <kirya.shvarts@list.ru>
2023-06-28 11:02:12 +0300

    Finish init

--- Chapter_2/Chapter_2.py
+++ Chapter_2/Chapter_2.py
@@ -1,229 +1,229 @@
-***Algorithmic simulator***
-Task 1
-height = input("Write your height:")
-print(height)
-Task 2
-color = input("Write your favorite color:")
-print(color)
-Task 3
-a = 3
-b = a + 2
-print(b)
-
-b = 4
-a = b * 4
-print(a)
-
-a = 5
-b = a / 3.14
-print(b)
-
-b = 15
-a = b - 8
-print(a)
-Task 4
-w = 5
-x = 4
-y = 8
-z = 2
-result = x + y
-result = z * 2
-result = y / x
-result = y - z
-result = w // z
-print(type(result), result)
-Task 5
-total = 10 + 14
-print(total)
-Task 6
-down_payment = 330
-total = 1000
-due = total - down_payment
-print(due)
-Task 7
-subtotal = 100.75
-total = subtotal * 0.15
-print (total)
-Task 8
-a = 5
-b = 2
-c = 3
-result = a + b * c
-print(result)
-Task 9
-num = 99
-num = 5
-print(num)
-Task 10
-sales = 12345.12345
-print(format(sales, ".2f"))
-Task 11
-number = 1234567.456
-print(format(number, ",.1f"))
-Task 12
-print('Джордж', 'Джон', 'Пол', 'Ринго', sep='@')
-Task 13
-import turtle
-turtle.circle(75)
-Task 14
-import turtle
-turtle.fillcolor('blue')
-turtle.begin_fill()
-turtle.forward(100)
-turtle.dot()
-turtle.setheading(90)
-turtle.forward(100)
-turtle.dot()
-turtle.setheading(180)
-turtle.forward(100)
-turtle.dot()
-turtle.setheading(270)
-turtle.forward(100)
-turtle.dot()
-turtle.setheading(360)
-turtle.end_fill()
-Task 15
-import turtle
-turtle.forward(100)
-turtle.dot()
-turtle.setheading(90)
-turtle.forward(100)
-turtle.dot()
-turtle.setheading(180)
-turtle.forward(100)
-turtle.dot()
-turtle.setheading(270)
-turtle.forward(100)
-turtle.dot()
-turtle.setheading(360)
-turtle.forward(50)
-turtle.dot()
-turtle.fillcolor('red')
-turtle.begin_fill()
-turtle.circle(50)
-turtle.end_fill()
-***Programming tasks***
-Task 1
-name = input("Write your name")
-address = input("Write your address, with your town, area and postal code")
-telephone = input("Write your telephone number")
-specialization = input("Write your specialization in your educational institution")
-print(name)
-print(address)
-print(telephone)
-print(specialization)
-Task 2
-total_sales_amount = float(input("Write total sales amount"))
-int(total_sales_amount)
-profit = total_sales_amount * 0.23
-print("Total profit is:", profit)
-Task 3
-Square_meters_of_land = float(input("Write total number of square meters of land"))
-print("Total number of acres is:", Square_meters_of_land / 4047)
-Task 4
-first_goods = float(input("Write cost of your first goods"))
-second_goods = float(input("Write cost of your second goods"))
-third_goods = float(input("Write cost of your third goods"))
-fourth_goods = float(input("Write cost of your fourth goods"))
-fifth_goods = float(input("Write cost of your fifth goods"))
-cost_of_all_goods = first_goods + second_goods + third_goods + fourth_goods + fifth_goods
-sales_tax = cost_of_all_goods * 0.7
-total_amount = cost_of_all_goods + sales_tax
-print("cost of all goods is:", cost_of_all_goods, "sales_tax is:", sales_tax, "total amount is:", total_amount)
-Task 5
-speed = 70
-time = float(input("Write time:"))
-distance = speed * time
-print("Distance is:", distance)
-Task 6
-purchase_amount = float(input("Write purchase amount:"))
-federal_tax = purchase_amount * 0.05
-regional_tax = purchase_amount * 0.025
-total_tax = federal_tax + regional_tax
-total_amount = purchase_amount + total_tax
-print("Purchase amount is:", purchase_amount, "\n federal tax is:", federal_tax, "\n regional tax is:", regional_tax)
-print("total amount is:", total_amount, "\n total tax is:", total_tax)
-Task 7
-kilometers_traveled = float(input("Write traveled kilometers:"))
-gasoline_consumption = float(input("Write gasoline consumption:"))
-print("Consumption is:", kilometers_traveled / gasoline_consumption)
-Task 8
-food_cost = float(input('Food cost is:'))
-tips = food_cost * 0.18
-tax = food_cost * 0.07
-total_cost = food_cost + tips + tax
-print('Food cost is:', food_cost, 'tips is:', tips, 'tax is:', tax, 'total_cost is:', total_cost)
-Task 9
-temperature_celsius = int(input('Write temperature in celsius:'))
-temperature_fahrenheit = 9/5 * temperature_celsius + 32
-print(temperature_fahrenheit)
-Task 10
-muffin = (int(input('Write amount of muffin')))
-print('Sugar is:', muffin * (1.5/48))
-print('Butter is:', muffin * (1/48))
-print('Flour is:', muffin * (2.75/48))
-Task 11
-boys = int(input('Write number of boys'))
-girls = int(input('Write number of girls'))
-students = boys + girls
-print('percent of boys is:', boys/students, 'percent of girls is:', girls/students)
-Task 12
-number_of_stocks = float(input('Write number of stocks:'))
-first_share_price = float(input('Write first share price:'))
-first_amount_of_money = number_of_stocks * first_share_price
-first_commission = first_amount_of_money * 0.03
-print('first share price is:', first_share_price, 'first commission is:', first_commission,)
-second_share_price = float(input('Write second share price:'))
-print('Second share price is:', second_share_price)
-second_amount_of_money = number_of_stocks * second_share_price
-second_commission = second_amount_of_money * 0.03
-total_amount_of_money = second_amount_of_money - first_commission - second_commission
-print('profit without any taxes is:', second_amount_of_money)
-print('clear profit is:', total_amount_of_money )
-Task 13
-ridge_length = float(input('Ridge length is:'))
-end_support_space = float(input('End support space is:'))
-space_between_vines = float(input('Space between vines is:'))
-amount_of_grapevine = (ridge_length - 2*end_support_space)/space_between_vines
-print('Amount of grapevine is:', amount_of_grapevine)
-Task 14
-amount_of_money = float(input('Amount of money is:'))
-annual_interest_rate = float(input('Annual interest rate is:'))
-Frequency_income_accrual = float(input('Frequency of income accrual'))
-number_of_years = float(input('Number of years of saving tax'))
-total_amount_of_money = amount_of_money*(1+annual_interest_rate/Frequency_income_accrual) ** (
-                        Frequency_income_accrual * number_of_years)
-print(total_amount_of_money)
-Task 15
-import turtle
-turtle.forward(200)
-turtle.write('Восток')
-turtle.right(180)
-turtle.forward(400)
-turtle.write('Запад')
-turtle.left(180)
-turtle.forward(200)
-turtle.left(90)
-turtle.forward(200)
-turtle.write('Север')
-turtle.left(180)
-turtle.forward(400)
-turtle.write('Юг')
-turtle.left(90)
-turtle.goto(0, -50)
-turtle.circle(50)
-turtle.done()
-
-import turtle
-turtle.fillcolor('red')
-turtle.begin_fill()
-turtle.goto(-100, -150)
-turtle.goto(-200, 0)
-turtle.goto(-100, 150)
-turtle.goto(0, 0)
-turtle.goto(100, 150)
-turtle.goto(200, 0)
-turtle.goto(100, -150)
-turtle.goto(0, 0)
-turtle.end_fill()
-turtle.done()
+# ***Algorithmic simulator***
+# Task 1
+# height = input("Write your height:")
+# print(height)
+# Task 2
+# color = input("Write your favorite color:")
+# print(color)
+# Task 3
+# a = 3
+# b = a + 2
+# print(b)
+#
+# b = 4
+# a = b * 4
+# print(a)
+#
+# a = 5
+# b = a / 3.14
+# print(b)
+#
+# b = 15
+# a = b - 8
+# print(a)
+# Task 4
+# w = 5
+# x = 4
+# y = 8
+# z = 2
+# result = x + y
+# result = z * 2
+# result = y / x
+# result = y - z
+# result = w // z
+# print(type(result), result)
+# Task 5
+# total = 10 + 14
+# print(total)
+# Task 6
+# down_payment = 330
+# total = 1000
+# due = total - down_payment
+# print(due)
+# Task 7
+# subtotal = 100.75
+# total = subtotal * 0.15
+# print (total)
+# Task 8
+# a = 5
+# b = 2
+# c = 3
+# result = a + b * c
+# print(result)
+# Task 9
+# num = 99
+# num = 5
+# print(num)
+# Task 10
+# sales = 12345.12345
+# print(format(sales, ".2f"))
+# Task 11
+# number = 1234567.456
+# print(format(number, ",.1f"))
+# Task 12
+# print('Джордж', 'Джон', 'Пол', 'Ринго', sep='@')
+# Task 13
+# import turtle
+# turtle.circle(75)
+# Task 14
+# import turtle
+# turtle.fillcolor('blue')
+# turtle.begin_fill()
+# turtle.forward(100)
+# turtle.dot()
+# turtle.setheading(90)
+# turtle.forward(100)
+# turtle.dot()
+# turtle.setheading(180)
+# turtle.forward(100)
+# turtle.dot()
+# turtle.setheading(270)
+# turtle.forward(100)
+# turtle.dot()
+# turtle.setheading(360)
+# turtle.end_fill()
+# Task 15
+# import turtle
+# turtle.forward(100)
+# turtle.dot()
+# turtle.setheading(90)
+# turtle.forward(100)
+# turtle.dot()
+# turtle.setheading(180)
+# turtle.forward(100)
+# turtle.dot()
+# turtle.setheading(270)
+# turtle.forward(100)
+# turtle.dot()
+# turtle.setheading(360)
+# turtle.forward(50)
+# turtle.dot()
+# turtle.fillcolor('red')
+# turtle.begin_fill()
+# turtle.circle(50)
+# turtle.end_fill()
+# ***Programming tasks***
+# Task 1
+# name = input("Write your name")
+# address = input("Write your address, with your town, area and postal code")
+# telephone = input("Write your telephone number")
+# specialization = input("Write your specialization in your educational institution")
+# print(name)
+# print(address)
+# print(telephone)
+# print(specialization)
+# Task 2
+# total_sales_amount = float(input("Write total sales amount"))
+# int(total_sales_amount)
+# profit = total_sales_amount * 0.23
+# print("Total profit is:", profit)
+# Task 3
+# Square_meters_of_land = float(input("Write total number of square meters of land"))
+# print("Total number of acres is:", Square_meters_of_land / 4047)
+# Task 4
+# first_goods = float(input("Write cost of your first goods"))
+# second_goods = float(input("Write cost of your second goods"))
+# third_goods = float(input("Write cost of your third goods"))
+# fourth_goods = float(input("Write cost of your fourth goods"))
+# fifth_goods = float(input("Write cost of your fifth goods"))
+# cost_of_all_goods = first_goods + second_goods + third_goods + fourth_goods + fifth_goods
+# sales_tax = cost_of_all_goods * 0.7
+# total_amount = cost_of_all_goods + sales_tax
+# print("cost of all goods is:", cost_of_all_goods, "sales_tax is:", sales_tax, "total amount is:", total_amount)
+# Task 5
+# speed = 70
+# time = float(input("Write time:"))
+# distance = speed * time
+# print("Distance is:", distance)
+# Task 6
+# purchase_amount = float(input("Write purchase amount:"))
+# federal_tax = purchase_amount * 0.05
+# regional_tax = purchase_amount * 0.025
+# total_tax = federal_tax + regional_tax
+# total_amount = purchase_amount + total_tax
+# print("Purchase amount is:", purchase_amount, "\n federal tax is:", federal_tax, "\n regional tax is:", regional_tax)
+# print("total amount is:", total_amount, "\n total tax is:", total_tax)
+# Task 7
+# kilometers_traveled = float(input("Write traveled kilometers:"))
+# gasoline_consumption = float(input("Write gasoline consumption:"))
+# print("Consumption is:", kilometers_traveled / gasoline_consumption)
+# Task 8
+# food_cost = float(input('Food cost is:'))
+# tips = food_cost * 0.18
+# tax = food_cost * 0.07
+# total_cost = food_cost + tips + tax
+# print('Food cost is:', food_cost, 'tips is:', tips, 'tax is:', tax, 'total_cost is:', total_cost)
+# Task 9
+# temperature_celsius = int(input('Write temperature in celsius:'))
+# temperature_fahrenheit = 9/5 * temperature_celsius + 32
+# print(temperature_fahrenheit)
+# Task 10
+# muffin = (int(input('Write amount of muffin')))
+# print('Sugar is:', muffin * (1.5/48))
+# print('Butter is:', muffin * (1/48))
+# print('Flour is:', muffin * (2.75/48))
+# Task 11
+# boys = int(input('Write number of boys'))
+# girls = int(input('Write number of girls'))
+# students = boys + girls
+# print('percent of boys is:', boys/students, 'percent of girls is:', girls/students)
+# Task 12
+# number_of_stocks = float(input('Write number of stocks:'))
+# first_share_price = float(input('Write first share price:'))
+# first_amount_of_money = number_of_stocks * first_share_price
+# first_commission = first_amount_of_money * 0.03
+# print('first share price is:', first_share_price, 'first commission is:', first_commission,)
+# second_share_price = float(input('Write second share price:'))
+# print('Second share price is:', second_share_price)
+# second_amount_of_money = number_of_stocks * second_share_price
+# second_commission = second_amount_of_money * 0.03
+# total_amount_of_money = second_amount_of_money - first_commission - second_commission
+# print('profit without any taxes is:', second_amount_of_money)
+# print('clear profit is:', total_amount_of_money )
+# Task 13
+# ridge_length = float(input('Ridge length is:'))
+# end_support_space = float(input('End support space is:'))
+# space_between_vines = float(input('Space between vines is:'))
+# amount_of_grapevine = (ridge_length - 2*end_support_space)/space_between_vines
+# print('Amount of grapevine is:', amount_of_grapevine)
+# Task 14
+# amount_of_money = float(input('Amount of money is:'))
+# annual_interest_rate = float(input('Annual interest rate is:'))
+# Frequency_income_accrual = float(input('Frequency of income accrual'))
+# number_of_years = float(input('Number of years of saving tax'))
+# total_amount_of_money = amount_of_money*(1+annual_interest_rate/Frequency_income_accrual) ** (
+#                         Frequency_income_accrual * number_of_years)
+# print(total_amount_of_money)
+# Task 15
+# import turtle
+# turtle.forward(200)
+# turtle.write('Восток')
+# turtle.right(180)
+# turtle.forward(400)
+# turtle.write('Запад')
+# turtle.left(180)
+# turtle.forward(200)
+# turtle.left(90)
+# turtle.forward(200)
+# turtle.write('Север')
+# turtle.left(180)
+# turtle.forward(400)
+# turtle.write('Юг')
+# turtle.left(90)
+# turtle.goto(0, -50)
+# turtle.circle(50)
+# turtle.done()
+#
+# import turtle
+# turtle.fillcolor('red')
+# turtle.begin_fill()
+# turtle.goto(-100, -150)
+# turtle.goto(-200, 0)
+# turtle.goto(-100, 150)
+# turtle.goto(0, 0)
+# turtle.goto(100, 150)
+# turtle.goto(200, 0)
+# turtle.goto(100, -150)
+# turtle.goto(0, 0)
+# turtle.end_fill()
+# turtle.done()
