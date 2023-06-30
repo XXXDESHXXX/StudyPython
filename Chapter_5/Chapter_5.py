131da018aa1c1eb1f22142474ffff9cce4cf9e7a
XXXDESHXXX <kirya.shvarts@list.ru>
2023-06-30 14:05:59 +0300

    Task 8

--- Chapter_5/Chapter_5.py
+++ Chapter_5/Chapter_5.py
@@ -1,5 +1,88 @@
 # ***Algorithmic simulator***
 # Task 1
-def times_ten(number):
-    return number * 10
+# def main():
+#     number = int(input('Input number: '))
+#     times_ten(number)
+#
+#
+# def times_ten(number):
+#     result = number * 10
+#     print(result)
+#
+#
+# main()
+# Task 2
+# def main():
+#     show_value(12)
+#
+#
+# def show_value(quantity):
+#     print(quantity)
+#
+#
+# main()
+# Task 3
+# def main():
+#     my_function(3, 2, 1)
+#
+#
+# def my_function(a, b, c):
+#     print(a, b, c)
+#
+#
+# main()
+# Task 4
+# def main():
+#     x = 1
+#     y = 3.4
+#     print(x, y)
+#     change_us(x, y)
+#     print(x, y)
+#
+#
+# def change_us(a, b):
+#     a = 0
+#     b = 0
+#     print(a, b)
+#
+#
+# main()
+# Task 5
+# def main():
+#     my_function(2, 4, 6)
+#
+#
+# def my_function(a, b, c):
+#     d = (a + c) / b
+#     print(d)
+#
+#
+# main()
+# Task 6
+# import random
+#
+#
+# def main():
+#     generic_function()
+#
+#
+# def generic_function():
+#     rand = random.randint(1, 100)
+#     print(rand)
+#
+#
+# main()
+# Task 7
+# def main():
+#     number = float(input('Input number: '))
+#     result = half(number)
+#     print(result)
+#
+#
+# def half(number):
+#     return number / 2
+#
+#
+# main()
+# Task 8
 
