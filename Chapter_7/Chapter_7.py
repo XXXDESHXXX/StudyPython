cf10ca11ec4e4ede05d7a41b2883ab10d46316f5
XXXDESHXXX <kirya.shvarts@list.ru>
2023-07-11 20:40:50 +0300

    Task 8

---
+++ Chapter_7/Chapter_7.py
@@ -0,0 +1,65 @@
+# ***Algorithmic simulator***
+# Task 1
+# def main():
+#     my_list = ['Einstein', 'Newton', 'Copernicus', 'Kepler']
+#     print(my_list)
+#
+#
+# main()
+# Task 2
+# def main():
+#     names = [1, 2, 3, 4, 5, 6, 9, 11, 222]
+#     for number in names:
+#         print(number)
+#
+#
+# main()
+# Task 3
+# import random
+#
+#
+# def main():
+#     iteration = 0
+#     element = random.randint(1, 101)
+#     numbers1 = []
+#     while iteration != 100:
+#         iteration += 1
+#         numbers1.append(element)
+#         element = random.randint(1, 100)
+#     numbers2 = [numbers1]
+#     print(numbers2)
+#
+#
+# main()
+# Task 5
+# def main():
+#     my_list = [1, 3, 44, 55, 100]
+#     result = sum_list(my_list)
+#     print(result)
+#
+#
+# def sum_list(my_list):
+#     total = 0
+#     for value in my_list:
+#         total += value
+#     return total
+#
+#
+# main()
+# Task 6
+# def main():
+#     my_list = ['Rubi', 'Anton', 'Artem']
+#     search = input('Input name that you want find: ')
+#     if search in my_list:
+#         print(search, 'is found!')
+#     else:
+#         print('This name not found')
+#
+#
+# main()
+# Task 7
+# list1 = [40, 50, 60]
+# list2 = [10, 20, 30]
+# list3 = list1 + list2
+# print(list3)
+# Task 8
