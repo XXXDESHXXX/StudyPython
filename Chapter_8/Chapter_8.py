8ad1853647cf539fbe147ec9166699b2c1b05a94
XXXDESHXXX <kirya.shvarts@list.ru>
2023-08-01 14:46:00 +0300

    Task 2

---
+++ Chapter_8/Chapter_8.py
@@ -0,0 +1,101 @@
+# ***Algorithmic simulator***
+# Task 1
+# def main():
+#     choice = input('Input your choice: ')
+#     if choice.upper() and choice.lower() == 'y' or 'Y':
+#         print('Yes')
+#
+#
+# main()
+# Task 2
+# def main():
+#     count_space = 0
+#     mystring = input('Input string: ')
+#     for letter in mystring:
+#         if letter.isspace():
+#             count_space += 1
+#     print(count_space)
+#
+#
+# main()
+# Task 3
+# def main():
+#     count_digit = 0
+#     mystring = input('Input string: ')
+#     for letter in mystring:
+#         if letter.isdigit():
+#             count_digit += 1
+#     print(count_digit)
+#
+#
+# main()
+# Task 4
+# def main():
+#     count_lower = 0
+#     mystring = input('Input string: ')
+#     for letter in mystring:
+#         if letter.islower():
+#             count_lower += 1
+#     print(count_lower)
+#
+#
+# main()
+# Task 5
+# def main():
+#     mystring = input('Input string: ')
+#     if mystring.endswith('.com'):
+#         print('True')
+#     else:
+#         print('False')
+#
+#
+# main()
+# Task 6
+# def main():
+#     mystring = input('Input string: ')
+#     new_string = mystring.replace('d', 'D')
+#     print(new_string)
+#
+#
+# main()
+# Task 7
+# def main():
+#     mystring = input('Input string: ')[::-1]
+#     print(mystring)
+#
+#
+# main()
+# Task 8
+# def main():
+#     mystring = input('Input string: ')
+#     print(mystring[:3])
+#
+#
+# main()
+# Task 9
+# def main():
+#     mystring = input('Input string: ')
+#     new_string = mystring[-3:]
+#     print(new_string)
+#
+#
+# main()
+# Task 10
+# def main():
+#     mystring = 'cakes>milk>apple>bananas>STAA$$'
+#     new_string = mystring.split('>')
+#     print(new_string)
+#
+#
+# main()
+# ***Programming tasks***
+# Task 1
+# def main():
+#     initial = ""
+#     mystring = input('Input your full name: ')
+#     for letter in mystring.split():
+#         initial += letter[:1] + "."
+#     print(initial)
+#
+#
+# main()
