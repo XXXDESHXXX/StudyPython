1618aad074b11a2aa33d487f5c99812127b0afa4
XXXDESHXXX <kirya.shvarts@list.ru>
2023-08-03 00:47:06 +0300

    Task 8

--- Chapter_8/Chapter_8.py
+++ Chapter_8/Chapter_8.py
@@ -99,3 +99,126 @@
 #
 #
 # main()
+# Task 2
+# def main():
+#     sum_of_numbers = 0
+#     my_string = input('Input string of numbers: ')
+#     for value in my_string:
+#         sum_of_numbers += int(value)
+#     print(sum_of_numbers)
+#
+#
+# main()
+# Task 3
+# def main():
+#     date = input('Input date in format dd/mm/yyyy: ')
+#     months = ["January", "February", "March", "April", "May",
+#               "June", "July", "August", "September", "October",
+#               "November", "December"]
+#     date_list = date.split('/')
+#     new_date = str(months[int(date_list[0]) - 1]) + " " + str(
+#         date_list[1]) + "," + " " + str(date_list[2])
+#     print(new_date)
+#
+#
+# main()
+# Task 4
+# def main():
+#     character = (" ", ",", ".", "?", "0", "1", "2", "3", "4", "5", "6", "7",
+#                  "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+#                  "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+#                  "W", "X", "Y", "Z")
+#     morse = (" ", "--..--", ".-.-.-", "..--..", "-----", ".----", "..---",
+#              "...--", "....-", ".....", "-....", "--...", "---..", "----.",
+#              ".-", "-...",
+#              "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-",
+#              ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-",
+#              "..-", "...-", ".--", "-..-",
+#              "-.-", "--..")
+#     string = input('Input string: ')
+#     index = 0
+#     while index < len(string):
+#         position = character.index(string[index])
+#         print(morse[position],
+#               end=" ")
+#         index += 1
+#
+#
+# main()
+# Task 5
+# def main():
+#     telephone_number = input('Input your telephone number: ')
+#     for ch in telephone_number:
+#         if ch.islower():
+#             ch = ch.upper()
+#         if ch.isdigit():
+#             print(ch, end='')
+#         elif ch == '-':
+#             print('-', end='')
+#         elif ch == 'A' or ch == 'B' or ch == 'C':
+#             print('2', end='')
+#         elif ch == 'D' or ch == 'E' or ch == 'F':
+#             print('3', end='')
+#         elif ch == 'G' or ch == 'H' or ch == 'I':
+#             print('4', end='')
+#         elif ch == 'J' or ch == 'K' or ch == 'L' \
+#                 or ch == 'V':
+#             print('5', end='')
+#         elif ch == 'M' or ch == 'N' or ch == 'O' \
+#                 or ch == 'U':
+#             print('6', end='')
+#         elif ch == 'P' or ch == 'Q' or ch == 'R' \
+#                 or ch == 'S' or ch == 'T':
+#             print('7', end='')
+#         elif ch == 'W' or ch == 'X' or ch == 'Y' \
+#                 or ch == 'Z':
+#             print('8', end='')
+#         else:
+#             print('-', end='')
+#
+#
+# main()
+# Task 6
+# def main():
+#     word_counter = 1
+#     text_file = open('Text.txt', 'r')
+#     content = text_file.readline()
+#     num_string = 0
+#     while num_string < len(content):
+#         content_split = content.split()
+#         word_counter += len(content.split())
+#         content = text_file.readline()
+#         num_string += 1
+#     median_of_words = word_counter / num_string
+#     print(median_of_words)
+#
+#
+# main()
+# Task 7
+# def main():
+#     upper_counter = 0
+#     lower_counter = 0
+#     number_counter = 0
+#     space_counter = 0
+#     text_file = open('Text.txt', 'r')
+#     content = text_file.readlines()
+#     for string in content:
+#         for ch in string:
+#             if ch.islower():
+#                 lower_counter += 1
+#             if ch.isalnum():
+#                 number_counter += 1
+#             if ch.isspace():
+#                 space_counter += 1
+#             if ch.isupper():
+#                 upper_counter += 1
+#     print(upper_counter, lower_counter, number_counter, space_counter)
+#
+#
+# main()
+# Task 8
+def main():
+
+
+
+main()
