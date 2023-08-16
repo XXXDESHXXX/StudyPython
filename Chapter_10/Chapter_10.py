84789f48656151fb02ba1524a0c74687ee1e8bad
XXXDESHXXX <kirya.shvarts@list.ru>
2023-08-16 23:48:58 +0300

    Task 2

---
+++ Chapter_10/Chapter_10.py
@@ -0,0 +1,56 @@
+# ***Algorithmic simulator***
+# Task 1
+# class Car:
+#     def __init__(self):
+#         self.way = 30
+#
+#     def go(self):
+#         return self.way + 1
+#
+#
+# def main():
+#     my_car = Car()
+#     my_car.go()
+#     print(my_car.go())
+#
+#
+# main()
+# Task 2
+# class Book:
+#     def __init__(self, name, author, publisher):
+#         self.name = name
+#         self.author = author
+#         self.publisher = publisher
+#
+#     def set_book_name(self, name):
+#         self.name = name
+#
+#     def set_book_author(self, author):
+#         self.author = author
+#
+#     def set_book_publisher(self, publisher):
+#         self.publisher = publisher
+#
+#     def get_book_name(self):
+#         return self.name
+#
+#     def get_book_author(self):
+#         return self.author
+#
+#     def get_book_publisher(self):
+#         return self.publisher
+#
+#
+# def main():
+#     name = input('Enter book name: ')
+#     author = input('Enter book author: ')
+#     publisher = input('Enter book publisher: ')
+#     book = Book(name, author, publisher)
+#     print(book.get_book_name())
+#     print(book.get_book_author())
+#     print(book.get_book_publisher())
+#
+#
+# main()
+# ***Programming tasks***
+# Task 1
