22c579807196f94fda5731a7c6f4d30d6dfa35c1
XXXDESHXXX <kirya.shvarts@list.ru>
2023-08-28 23:52:38 +0300

    Task 1

---
+++ Chapter_11/Chapter_11.py
@@ -0,0 +1,28 @@
+# ***Algorithmic simulator***
+# Task 2
+# class Plant:
+#     def __init__(self, plant_type):
+#         self._plant_type = plant_type
+#
+#     def message(self):
+#         print("Я - планета.")
+#
+#
+# class Tree(Plant):
+#     def init(self):
+#         Plant.__init__(self, 'дерево')
+#
+#     def message(self):
+#         print("Я - дерево.")
+#
+#
+# def main():
+#     p = Plant('саженец')
+#     t = Tree('123')
+#     p.message()
+#     t.message()
+#
+#
+# main()
+# ***Programming tasks***
+# Task 1
